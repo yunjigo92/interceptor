0d67b60117469411a8aff494721fff862178844f
yj.ko <yunjigo92@gmail.com>
2022-05-09 18:45:26 +0900

    add interceptor 예제 실습

---
+++ src/main/java/com/yunji/interceptor/interceptor/AuthInterceptor.java
@@ -0,0 +1,18 @@
+package com.yunji.interceptor.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String url = request.getRequestURI();
+
+
+        return false;
+    }
+}
---
+++ src/test/java/com/yunji/interceptor/InterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunji.interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
